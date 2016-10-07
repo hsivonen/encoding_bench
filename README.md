184a5fb446d39eec93bd845d8ecb9264e0ff4388
Henri Sivonen <hsivonen@hsivonen.fi>
2016-10-07 11:33:04 +0300

    Add a basic bench.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a performance testing framework for understanding the performance
 characteristics of [encoding_rs](https://github.com/hsivonen/encoding_rs).
 
 This framework is separate from encoding_rs itself in order to separate
-nightly-only Rust features (`#bench`) and CC-licensed test data from the main
+nightly-only Rust features (`#[bench]`) and CC-licensed test data from the main
 repository.
 
 ## Licensing
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+#![feature(test)]
+
+extern crate test;
+extern crate encoding_rs;
+
+use test::Bencher;
+
+const OUTPUT_BUFFER_LENGTH: usize = 512 * 1024;
+
+#[bench]
+fn bench_decode_to_utf8_en(b: &mut Bencher) {
+    let input = include_bytes!("wikipedia/en.html");
+    let mut output = [0u8; OUTPUT_BUFFER_LENGTH];
+    b.bytes = input.len() as u64;
+    b.iter(|| {
+        let mut decoder = encoding_rs::UTF_8.new_decoder();
+        let (result, _, _, _) = decoder.decode_to_utf8(input, &mut output[..], true);
+        match result {
+            encoding_rs::CoderResult::InputEmpty => {}
+            encoding_rs::CoderResult::OutputFull => {
+                unreachable!("Output buffer too short.");
+            }
+        }
+        test::black_box(&output);
+    });
 }
